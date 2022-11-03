d92d356f55b9a1d112890d51ac8c8aa654887ea2
Justas Palumickas <jpalumickas@gmail.com>
2022-11-03 21:56:20 +0200

    Small fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,8 +7,8 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
-          name="title"
-          content="Justas Palumickas - Full Stack Development"
+          name="description"
+          content="Interested in open source and full stack development. Technologies I currently love and happy to work with - React, React Native, TypeScript/JavaScript, Node.js, GraphQL, Electron, Prisma and more."
         />
         <link
           rel="apple-touch-icon"
