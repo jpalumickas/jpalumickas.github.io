e1d94d7df141c87d3ef6772d5c2fa84397635083
Justas Palumickas <jpalumickas@gmail.com>
2022-11-03 20:13:45 +0200

    Add more content

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import Image from "next/image";
+
 export default function Page() {
-  return <h1 className="text-red-500">Hello, Next.js!</h1>;
+  return (
+    <div className="w-screen h-screen flex flex-col flex-1 justify-center items-center">
+      <div className="mb-4">
+        <Image
+          src="https://www.gravatar.com/avatar/6d5c3a1354b53442fe342b2189e76a7a?s=200"
+          alt="{{ site.author }}"
+          width="100"
+          height="100"
+          className="rounded-full"
+        />
+      </div>
+      <h1 className="text-4xl text-gray-400 font-extralight">
+        {"Hi, I'm "}
+        <span className="font-light text-gray-600">Justas Palumickas</span>
+      </h1>
+    </div>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.gravatar.com',
+        pathname: '/avatar/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
